62df2a5f7ffb4b877eaa2d2b89363eb42dfb5f4f
dreamerwu <haowbdreamer@gmail.com>
2017-09-25 10:31:12 -0400

    Update ANOVA and pheatmap plot.R

--- ANOVA and pheatmap plot.R
+++ ANOVA and pheatmap plot.R
@@ -6,13 +6,23 @@ ANOVA=aov(y~x,data=data)
 summary(ANOVA)
 
 
-######To plot these biomarkers
+######To plot mutation biomarkers
+library("pheatmap")
 data=read.delim("D:/demo/mutation_TEAD2_dependency.txt",head=T,sep="\t")
-ncol(data)
-data2=data[,2:14]
+data2=data[,2:ncol(data)]
 annotation=data$Name
 rownames(data2)=annotation  #Define the row name
 pheatmap(data2,cluster_cols=FALSE,cluster_rows=FALSE,color=colorRampPalette(c("white","black"))(50),border_color="gray",show_rownames=TRUE)
+
+
+#####To plot CNV biomarkers
+library("pheatmap")
+data=read.delim("D:/demo/CNV_TEAD2_dependency.txt",head=T,sep="\t")
+data2=data[,2:ncol(data)]
+annotation=data$Cell_line_name
+rownames(data2)=annotation  #Define the row name
+pheatmap(data2,cluster_cols=FALSE,cluster_rows=FALSE,color=colorRampPalette(c("green","white","red"))(50),border_color="gray",show_rownames=TRUE)
+
 
 
 
